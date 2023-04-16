5117989bcbeea7209fc6e129794c52ea0f465059
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-04-16 20:32:12 -0300

    starting alteration on exception handling.

--- backend/bookstore/src/main/java/com/poletto/bookstore/services/exceptions/InvalidStatus.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/services/exceptions/InvalidStatus.java
@@ -2,9 +2,13 @@ package com.poletto.bookstore.services.exceptions;
 
 import java.io.Serializable;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import com.poletto.bookstore.entities.Book;
 import com.poletto.bookstore.entities.Reservation;
 
+@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE, reason="Invalid status change")
 public class InvalidStatus extends RuntimeException implements Serializable {
 
 	private static final long serialVersionUID = 1L;
