944d51b821897f405b0f468065c0066cbe50eebe
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-07-15 20:46:27 -0300

    Habilitado metódos assíncronos nos metódos void.

--- backend/bookstore/src/main/java/com/poletto/bookstore/config/ApplicationConfig.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.poletto.bookstore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.poletto.bookstore.repositories.UserRepository;
 
 @Configuration
+@EnableAsync
 public class ApplicationConfig {
 
 	@Autowired
