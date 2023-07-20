fd9a0618ad0dfc0bfe0c898415475d9079601625
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-07-19 21:56:14 -0300

    Revertido url para request.ts

--- frontend/bookstore/src/utils/requests.ts
+++ frontend/bookstore/src/utils/requests.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import history from './history';
 import { getAuthData } from './storage';
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL + ":" + process.env.REACT_APP_PORT;
+export const BASE_URL = "https://192.168.15.13:8443"
 
 type LoginData = {
 
