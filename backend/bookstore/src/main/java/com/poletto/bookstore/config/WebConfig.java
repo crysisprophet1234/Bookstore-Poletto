cc96b3b3e7de9334b4378c7f48114ace1d89a75b
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-07-23 01:33:13 -0300

    Adicionado ObjectMapper customizado com javaTimeModule na webconfig.

--- backend/bookstore/src/main/java/com/poletto/bookstore/config/WebConfig.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/config/WebConfig.java
@@ -2,19 +2,35 @@ package com.poletto.bookstore.config;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poletto.bookstore.converter.CustomObjectMapper;
 import com.poletto.bookstore.serialization.converter.YamlJacksonToHttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
-
+	
+	@Bean(name = "jsonMapper")
+	@Primary
+	ObjectMapper jsonMapper() {
+		return new CustomObjectMapper();
+	}
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	  converters.add(new MappingJackson2HttpMessageConverter(jsonMapper()));
+	}
+	
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJacksonToHttpMessageConverter());
