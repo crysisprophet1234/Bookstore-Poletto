7034a559d2475a7bbef61b917c570112a12cee3b
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-07-25 20:36:50 -0300

    Removido verified account, vai ficar pra v3.

---
+++ backend/bookstore/src/main/resources/db/migrations/V2_1_0__Drop_User_Column.sql
@@ -0,0 +1,6 @@
+-- Column: public.bookstore_user.verified_account;
+
+-- ALTER TABLE IF EXISTS public.bookstore_user DROP COLUMN IF EXISTS verified_account;
+
+ALTER TABLE IF EXISTS public.bookstore_user
+    DROP COLUMN IF EXISTS verified_account;
