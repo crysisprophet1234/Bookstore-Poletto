7fd44c7d2783d6391278be5988b63754e1344fe7
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-07-25 23:58:54 -0300

    Padronizado consume/produce de Media Type  X-YAML.

--- backend/bookstore/src/main/java/com/poletto/bookstore/config/WebConfig.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/config/WebConfig.java
@@ -2,16 +2,13 @@ package com.poletto.bookstore.config;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poletto.bookstore.converter.CustomObjectMapper;
 import com.poletto.bookstore.serialization.converter.YamlJacksonToHttpMessageConverter;
 
@@ -20,15 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 	
-	@Bean(name = "jsonMapper")
-	@Primary
-	ObjectMapper jsonMapper() {
-		return new CustomObjectMapper();
-	}
-	
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-	  converters.add(new MappingJackson2HttpMessageConverter(jsonMapper()));
+	  converters.add(new MappingJackson2HttpMessageConverter(new CustomObjectMapper()));
 	}
 	
 	@Override
@@ -46,8 +37,7 @@ public class WebConfig implements WebMvcConfigurer {
 				.mediaType("json", MediaType.APPLICATION_JSON)
 				.mediaType("xml", MediaType.APPLICATION_XML)
 				.mediaType("text/plain", MediaType.TEXT_PLAIN)
-				.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML) //TODO quando existe data no response, o yaml n parsa
-				//.mediaType("format-data", MediaType.form) //verificar form url encoded
+				.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
 			.defaultContentType(MediaType.APPLICATION_JSON);
 
 	}
