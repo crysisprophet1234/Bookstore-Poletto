ab6aefa28cb5fb43051499d8d5160f93b7f52125
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-08-20 19:06:51 -0300

    Corrigido query do find reservation by bookId.

--- backend/bookstore/src/main/java/com/poletto/bookstore/repositories/ReservationRepository.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/repositories/ReservationRepository.java
@@ -9,8 +9,10 @@ import com.poletto.bookstore.entities.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-	@Query("SELECT res FROM Reservation res " + "LEFT JOIN BookReservation bookres "
-			+ "WHERE bookres.id.book.id = :id AND res.status NOT IN ('FINISHED')")
+	@Query("SELECT res FROM Reservation res " 
+		 + "LEFT JOIN BookReservation bookres "
+		 + "ON bookres.id.reservation.id = res.id "
+		 + "WHERE bookres.id.book.id = :id AND res.status NOT IN ('FINISHED')")
 	Reservation findByBook(Long id);
 
 	@Query("SELECT res FROM Reservation res WHERE res.client.id = :id")
