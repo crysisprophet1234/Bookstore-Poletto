9fdd4c19449155ad6711703c14800612e87e4ec6
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-08-24 18:32:33 -0300

    Editado estilo do container e removido path de página de reservas.

--- frontend/bookstore/src/pages/Admin/Reserves/index.tsx
+++ frontend/bookstore/src/pages/Admin/Reserves/index.tsx
@@ -1,20 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import List from "./List";
-import ReservationDetails from "./ReservationDetails";
 
 const Reserves = () => {
 
     return (
 
-        <div className='container-fluid mx-2'>
+        <div className='container-fluid'>
 
             <h2 className='mb-3' >Reservas</h2>
 
             <Routes>
 
                 <Route path="/" element={<List />} />
-
-                <Route path="/:reservationId" element={<ReservationDetails />} />
 
             </Routes>
 
