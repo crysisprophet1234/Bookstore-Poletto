90ab1571b1e5e3a167d350691ff447ac2c1a8717
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-08-29 22:31:16 -0300

    Alterados metódos de obtençaõ e comparação de autoridades no token.

--- frontend/bookstore/src/utils/auth.ts
+++ frontend/bookstore/src/utils/auth.ts
@@ -1,43 +1,51 @@
-import jwtDecode from 'jwt-decode';
-import { getAuthData } from './storage';
+import jwtDecode from 'jwt-decode'
+import { getAuthData } from './storage'
 
-export type Authority = 'ROLE_CUSTOMER' | 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+export type Authority = 'ROLE_CUSTOMER' | 'ROLE_OPERATOR' | 'ROLE_ADMIN'
 
 export type TokenData = {
-  exp: number;
-  iat: number;
-  sub: string;
-  authorities: Authority[];
-};
+
+  exp: number
+  iat: number
+  sub: string
+  authorities: Authority[]
+
+}
 
 export const getTokenData = (): TokenData | undefined => {
+
   try {
-    return jwtDecode(getAuthData().token) as TokenData;
+
+    return jwtDecode(getAuthData().token) as TokenData
+
   } catch (error) {
-    return undefined;
+
+    return undefined
+
   }
-};
+
+}
 
 export const isAuthenticated = (): boolean => {
-  let tokenData = getTokenData();
-  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
-};
 
-export const hasAnyAuthorities = (authorities: Authority[]): boolean => {
-  if (typeof (authorities) === 'undefined') {
-    return true;
+  let tokenData = getTokenData()
+
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false
+
+}
+
+export const hasAuthority = (authority: Authority): boolean => {
+
+  const authorities = getTokenData()?.authorities
+
+  if (authorities) {
+
+    return getTokenData()!.authorities.includes(authority)
+
+  } else {
+
+    return false
+
   }
 
-  const tokenAuthorities = getTokenData()?.authorities;
-
-  tokenAuthorities?.forEach(a => {
-    console.log(a)
-    console.log(authorities[1])
-    if (a.toString() === authorities[1]) {
-      return true;
-    }
-  })
-
-  return false;
-
-};
+}
