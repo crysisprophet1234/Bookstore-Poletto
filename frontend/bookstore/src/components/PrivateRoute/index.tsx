11b0231d81b19c8e9251dd0b3f8df61de81bef54
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-08-29 22:31:01 -0300

    Alterado lógica de proteção das rotas privadas no frontend.

--- frontend/bookstore/src/components/PrivateRoute/index.tsx
+++ frontend/bookstore/src/components/PrivateRoute/index.tsx
@@ -1,20 +1,48 @@
-import { Navigate, useLocation } from 'react-router-dom'
-import { isAuthenticated } from '../../utils/auth'
 import { ReactNode } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { Authority, hasAuthority, isAuthenticated } from '../../utils/auth'
+import NotAuthorized from '../NotAuthorized'
 
 type Props = {
   element: ReactNode
+  authority?: Authority
 }
 
-const PrivateRoute = ({ element }: Props) => {
+const PrivateRoute = ({ element, authority }: Props) => {
 
   const location = useLocation()
 
-  return (
+  function checkAuthority() {
 
-      isAuthenticated() ? <> {element} </> : <Navigate to='/auth/login' state={{ from: location }} /> 
+    if (authority) {
+      return hasAuthority(authority)
+    }
 
-  )
+    return true
+
+  }
+
+  if (!isAuthenticated()) {
+
+    return <Navigate to='/auth/login' state={{ from: location }} />
+
+  } else {
+
+    if (checkAuthority()) {
+
+      return <>{element}</>
+
+    }
+
+    if (!checkAuthority() && authority === 'ROLE_ADMIN') {
+
+      return <>{<NotAuthorized />}</>
+
+    }
+
+    return <Navigate to='/auth/login' state={{ from: location }} />
+
+  }
 
 }
 
