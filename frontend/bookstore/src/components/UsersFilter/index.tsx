5a562aae252772412d6c3daa94da8d064b37bd30
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-09-15 21:31:15 -0300

    Commit inicial frontend da página de Users.

---
+++ frontend/bookstore/src/components/UsersFilter/index.tsx
@@ -0,0 +1,226 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, useForm } from 'react-hook-form'
+import Select from 'react-select'
+import * as Yup from 'yup'
+import { Sort } from '../../types/vendor/spring'
+import { selectStyles } from '../../utils/selectStyles'
+
+import { Authority } from '../../utils/auth'
+import '../ReservesFilter/styles.css'
+
+export type UserFilterData = Sort & {
+
+    id?: string
+    name?: string
+    role?: Authority
+
+}
+
+type Props = {
+
+    onSubmitFilter: (data: UserFilterData) => void
+
+}
+
+const UsersFilter = ({ onSubmitFilter }: Props) => {
+
+    const validationSchema = Yup.object().shape({
+
+        id: Yup.string()
+            .notRequired()
+            .matches(/^(?:[1-9][0-9]*)?$/, 'Código não pode ser 0'),
+
+
+        name: Yup.string()
+            .notRequired()
+            .matches(/^(?=.*[a-zA-Z]).*|^\s*$/, 'Nome ou e-mail deve ser válido'),
+
+
+        orderBy: Yup.string()
+            .notRequired()
+
+    })
+
+    const formOptions = { resolver: yupResolver(validationSchema) }
+
+    const { register, handleSubmit, setValue, control, reset, formState: { errors } } = useForm<UserFilterData>(formOptions)
+
+    const onSubmit = (formData: UserFilterData) => {
+        console.log(formData)
+        onSubmitFilter(formData)
+    }
+
+    return (
+
+        <div className='base-card p-3 mb-2 filter-container'>
+
+            <form id='users-filter-form' onSubmit={handleSubmit(onSubmit)}>
+
+                <div className='filter-btn-container'>
+
+                    <Controller
+                        name='orderBy'
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                options={[
+                                    { label: 'Código', value: 'id' },
+                                    { label: 'Nome', value: 'name' },
+                                    { label: 'E-mail', value: 'email' }
+                                ]}
+                                isClearable
+                                className='select-order filter-right-btn'
+                                onChange={(selectedOption) => setValue('orderBy', selectedOption?.value)}
+                                value={undefined}
+                                getOptionLabel={(sort) => sort?.label}
+                                getOptionValue={(sort) => sort?.value}
+                                form='user-filter-form'
+                                placeholder='Ordenar por'
+                                styles={{ ...selectStyles }}
+                            />
+                        )}
+                    />
+
+                    <Controller
+                        name='sort'
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                options={[
+                                    { label: 'Crescente', value: 'asc' },
+                                    { label: 'Decrescente', value: 'desc' }
+                                ]}
+                                isClearable
+                                className='select-order filter-right-btn'
+                                onChange={(selectedOption) => setValue('sort', selectedOption?.value)}
+                                value={undefined}
+                                getOptionLabel={(sort) => sort?.label}
+                                getOptionValue={(sort) => sort?.value}
+                                placeholder='Ordem'
+                                form='user-filter-form'
+                                styles={{ ...selectStyles }}
+                            />
+                        )}
+                    />
+
+                </div>
+
+                <div className='my-4 double-input-container'>
+
+                    <div style={{ flex: '.15' }}>
+
+                        <label htmlFor='id'>Código</label>
+
+                        <input
+                            {...register('id', {
+                                required: false
+                            })}
+                            type='number'
+                            className={`form-control base-input ${errors.id ? 'is-invalid' : ''}`}
+                            placeholder='Código do usuário'
+                            form='users-filter-form'
+                            id='id'
+                            name='id'
+                            autoComplete='off'
+                        />
+                        <div className='invalid-feedback d-block'>
+                            {errors.id?.message}
+                        </div>
+
+                    </div>
+
+                    <div style={{ flex: '.34' }}>
+
+                        <label htmlFor='role'>Nível de acesso</label>
+
+                        <Controller
+                            name='role'
+                            control={control}
+                            rules={{ required: false }}
+                            render={({ field }) => (
+                                <Select
+                                    {...field}
+                                    options={[
+                                        { label: 'Administrador', value: 'ROLE_ADMIN' },
+                                        { label: 'Operador', value: 'ROLE_OPERATOR' }
+                                    ]}
+                                    isClearable
+                                    value={control._defaultValues.role}
+                                    onChange={(role) => setValue('role', role?.value)}
+                                    getOptionLabel={(role) => role?.label}
+                                    getOptionValue={(role) => role?.value}
+                                    className='filter-select'
+                                    classNamePrefix='filter-select'
+                                    form='users-filter-form'
+                                    name='role'
+                                    placeholder='Nível de acesso'
+                                    id='role'
+                                    styles={selectStyles}
+                                />
+                            )}
+                        />
+
+                    </div>
+
+                </div>
+
+                <div className='mb-4 double-input-container'>
+
+                    <div style={{ flex: '.5' }}>
+
+                        <label htmlFor='name'>Nome ou e-mail</label>
+
+                        <input
+                            {...register('name', {
+                                required: false
+                            })}
+                            type='text'
+                            className={`form-control base-input ${errors.name ? 'is-invalid' : ''}`}
+                            placeholder='Nome ou e-mail do usuário'
+                            form='users-filter-form'
+                            id='name'
+                            name='name'
+                            autoComplete='username'
+                        />
+                        <div className='invalid-feedback d-block'>
+                            {errors.name?.message}
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </form>
+
+            <div className='buttons-container mb-3'>
+
+                <button
+                    onClick={e => {
+                        reset()
+                    }}
+                    type='button'
+                    className='btn btn-outline-dark btn-product-filter'
+                    form='users-filter-form'
+                >
+                    LIMPAR FILTRO
+                </button>
+
+                <button
+                    type='submit'
+                    className='btn btn-outline-primary btn-product-filter mx-4'
+                    form='users-filter-form'
+                >
+                    PESQUISAR
+                </button>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default UsersFilter
--- frontend/bookstore/src/pages/Admin/Users/index.tsx
+++ frontend/bookstore/src/pages/Admin/Users/index.tsx
@@ -1,92 +1,27 @@
-import { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
-import { useEffect, useState } from 'react';
-import { User } from '../../../types/user';
-import { SpringPage } from '../../../types/vendor/spring';
-import { requestBackend } from '../../../utils/requests';
-import './styles.css'
-import { useNavigate } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom"
+import List from "./List"
+
+//const UserPage = lazy(() => import('path-to-userpage'))
 
 const Users = () => {
 
-    const [page, setPage] = useState<SpringPage<User>>();
-
-    const history = useNavigate()
-
-    useEffect(() => {
-
-        const params: AxiosRequestConfig = {
-
-            url: '/api/users/v2',
-            withCredentials: true,
-            headers: {} as AxiosRequestHeaders,
-            params: {
-                page: 0,
-                size: 200,
-            },
-
-        };
-
-        requestBackend(params)
-            .then((response) => {
-                setPage(response.data);
-            })
-            .catch((err) => {
-            })
-
-    }, []);
-
-    const handleDelete = (bookId: number) => {
-
-        if (!window.confirm(`Tem certeza que deseja deletar usuário ${bookId}?`)) {
-            return;
-        }
-
-        const config: AxiosRequestConfig = {
-            method: 'DELETE',
-            url: `/api/users/v2/${bookId}`,
-            withCredentials: true,
-        };
-
-        requestBackend(config).then(() => {
-            history('/admin/users')
-        });
-    };
-
     return (
 
-        <div className="user-container">
+        <div className='container-fluid'>
 
-            {true ?
+            <h2 className='mb-3' >Usuários</h2>
 
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>EMAIL</th>
-                        <th>EXCLUIR</th>
-                    </tr>
+            <Routes>
 
-                    <tbody>
+                <Route path="/" element={<List />} />
 
-                        {page?.content.map((item) => (
-                            <>
-                                <tr>
-                                    <td>{item.id}</td>
-                                    <td>{item.email}</td>
-                                    <td><button type='button' onClick={() => handleDelete(item.id)}>X</button></td>
-                                </tr>
-                            </>
-                        ))}
+                {/* <Route path="/:id" element={<UserPage />} /> */}
 
-                    </tbody>
-                </table>
-
-                : <h1>Usuário deve ser administrador para acessar</h1>
-
-            }
+            </Routes>
 
         </div>
 
-    );
-};
+    )
+}
 
-export default Users;
+export default Users
