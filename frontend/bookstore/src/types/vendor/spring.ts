e91886c7108e0c43dc6f8cfa0324c6723e3f1e1c
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-09-15 21:30:20 -0300

    exportado type sort em SpringPage

--- frontend/bookstore/src/types/vendor/spring.ts
+++ frontend/bookstore/src/types/vendor/spring.ts
@@ -1,13 +1,18 @@
 export type SpringPage<T> = {
 
-    content: T[];
-    last: boolean;
-    totalElements: number;
-    totalPages: number;
-    size: number;
-    number: number;
-    first: boolean;
-    numberOfElements?: number;
-    empty: boolean;
+    content: T[]
+    last: boolean
+    totalElements: number
+    totalPages: number
+    size: number
+    number: number
+    first: boolean
+    numberOfElements?: number
+    empty: boolean
 
+}
+
+export type Sort = {
+    sort: string
+    orderBy: string
 }
