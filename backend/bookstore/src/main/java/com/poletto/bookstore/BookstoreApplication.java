ce735d3b5d0c75c4b94ae240d532e0c02fd538b0
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-09-26 22:04:24 -0300

    Habilitando caching na Main #20

--- backend/bookstore/src/main/java/com/poletto/bookstore/BookstoreApplication.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/BookstoreApplication.java
@@ -2,8 +2,10 @@ package com.poletto.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
