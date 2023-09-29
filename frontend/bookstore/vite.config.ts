209808ae807d10382789f875772b220f1f106706
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-09-28 21:30:14 -0300

    Conversão de CRA para Vite@SWC + Vitest.

---
+++ frontend/bookstore/vite.config.ts
@@ -0,0 +1,37 @@
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        outDir: 'build'
+    },
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '@images': path.resolve(__dirname, 'src/assets/images')
+        }
+    },
+    server: {
+        port: 3000
+    },
+    base: '/',
+    plugins: [
+        react(),
+        svgr()
+    ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        css: true,
+        reporters: ['verbose'],
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+            include: ['src/**/*'],
+            exclude: [],
+        }
+    },
+})
