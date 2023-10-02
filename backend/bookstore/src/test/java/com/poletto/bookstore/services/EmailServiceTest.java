daad1a403fcf3ccfc922feb65391da263875d3b9
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-01 21:15:55 -0300

    Refatorado teste do EmailService.

--- backend/bookstore/src/test/java/com/poletto/bookstore/services/EmailServiceTest.java
+++ backend/bookstore/src/test/java/com/poletto/bookstore/services/EmailServiceTest.java
@@ -8,48 +8,42 @@ import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import com.poletto.bookstore.services.v2.EmailService;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class EmailServiceTest {
-	
-	private static final Logger logger = LoggerFactory.getLogger(EmailServiceTest.class);
 
-    @Mock
+    @SpyBean
     private JavaMailSender mailSender;
 
     @InjectMocks
+    @Autowired
     private EmailService emailService;
 
     @Test
     public void testSendEmail() {
-        // Prepare test data
-        String to = "ultimateleodib@gmail.com";
-        String subject = "Test Subject";
-        String body = "Test Body Right now is: " + new Date();
 
-        // Define expected behavior
+        String to = "polettobookstore@gmail.com";
+        String subject = "Test Subject";
+        String body = "Test Body, right now is: " + new Date();
+
         SimpleMailMessage expectedMessage = new SimpleMailMessage();
         expectedMessage.setTo(to);
         expectedMessage.setSubject(subject);
         expectedMessage.setText(body);
 
-        // Mock the mail sender and verify the message is sent
         emailService.sendEmail(to, subject, body);
         verify(mailSender, times(1)).send(expectedMessage);
         
-        logger.info("EMAIL SENT {\"From\": \"" 		+ expectedMessage.getFrom() 	+
-        			"\", \"To\": \"" 		+ expectedMessage.getTo() 		+
-        			"\", \"Subject\": \"" 	+ expectedMessage.getSubject()	+
-        			"\", \"Body\": \"" 		+ expectedMessage.getText()		+ "\"}");
-        
     }
+        
 }
 
