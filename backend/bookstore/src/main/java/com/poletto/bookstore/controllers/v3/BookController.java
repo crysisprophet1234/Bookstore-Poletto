2a80e543a3b249c62776a4ebbd16375b6f3183d3
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-04 22:19:47 -0300

    Iniciado versionamento v3 das services e controllers com caching. #20

---
+++ backend/bookstore/src/main/java/com/poletto/bookstore/controllers/v3/BookController.java
@@ -0,0 +1,78 @@
+package com.poletto.bookstore.controllers.v3;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.poletto.bookstore.dto.v2.BookDTOv2;
+import com.poletto.bookstore.services.v3.BookService;
+
+import jakarta.validation.Valid;
+
+@RestController("BookControllerV3")
+@RequestMapping(value = "/books/v3")
+public class BookController {
+
+	@Autowired
+	private BookService bookService;
+	
+	@GetMapping
+	public ResponseEntity<Page<BookDTOv2>> findAllPaged(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "sort", defaultValue = "asc") String sort,
+			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy,
+			@RequestParam(value = "categoryId", defaultValue = "") Long categoryId,
+			@RequestParam(value = "name", defaultValue = "") String name,
+			@RequestParam(value = "status", defaultValue = "all") String status) {
+
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Direction.valueOf(sort.toUpperCase()), orderBy));
+		
+		Page<BookDTOv2> books = bookService.findAllPaged(pageable, categoryId, name.trim(), status);
+
+		return ResponseEntity.ok(books);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<BookDTOv2> findById(@PathVariable Long id) {
+		BookDTOv2 entity = bookService.findById(id);
+		return ResponseEntity.ok().body(entity);
+	}
+	
+	@PostMapping
+	public ResponseEntity<BookDTOv2> insert(@RequestBody @Valid BookDTOv2 dto) {
+		dto = bookService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<BookDTOv2> update(@PathVariable Long id, @RequestBody @Valid BookDTOv2 dto) {
+		BookDTOv2 bookDTO = bookService.update(id, dto);
+		return ResponseEntity.ok().body(bookDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		bookService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ backend/bookstore/src/main/java/com/poletto/bookstore/services/v3/BookService.java
@@ -0,0 +1,196 @@
+package com.poletto.bookstore.services.v3;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.poletto.bookstore.controllers.v2.BookController;
+import com.poletto.bookstore.converter.custom.BookMapper;
+import com.poletto.bookstore.dto.v2.BookDTOv2;
+import com.poletto.bookstore.dto.v2.CategoryDTOv2;
+import com.poletto.bookstore.entities.Book;
+import com.poletto.bookstore.entities.Category;
+import com.poletto.bookstore.entities.enums.BookStatus;
+import com.poletto.bookstore.exceptions.DatabaseException;
+import com.poletto.bookstore.exceptions.ResourceNotFoundException;
+import com.poletto.bookstore.repositories.v2.AuthorRepository;
+import com.poletto.bookstore.repositories.v2.BookRepository;
+import com.poletto.bookstore.repositories.v2.CategoryRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service("BookServiceV3")
+public class BookService {
+
+	// TODO fazer classes de teste para cache
+	
+	// TODO serializando categorias de livros em ordem diferente, criar issue dps
+
+	private static final Logger logger = LoggerFactory.getLogger(BookService.class);
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Cacheable(value = "books")
+	@Transactional(readOnly = true)
+	public Page<BookDTOv2> findAllPaged(Pageable pageable, Long categoryId, String name, String status) {
+
+		Page<Book> bookPage = bookRepository.findPaged(
+				categoryId,
+				name.toLowerCase(),
+				status.toUpperCase(),
+				pageable);
+
+		logger.info("Resource BOOK page found: PAGE NUMBER [" + bookPage.getNumber() + "] "
+				+ "- CONTENT: " + bookPage.getContent());
+
+		Page<BookDTOv2> dtosPage = bookPage.map(x -> BookMapper.convertEntityToDtoV2(x));
+
+		dtosPage.stream().forEach(x -> x
+				.add(linkTo(methodOn(BookController.class).findById(x.getId())).withSelfRel().withType("GET"))
+				.add(linkTo(methodOn(BookController.class).delete(x.getId())).withRel("delete").withType("DELETE"))
+				.add(linkTo(methodOn(BookController.class).update(x.getId(), x)).withRel("update").withType("PUT")));
+
+		return dtosPage;
+
+	}
+
+	@Cacheable(value = "book", key = "#id")
+	@Transactional(readOnly = true)
+	public BookDTOv2 findById(Long id) {
+
+		Optional<Book> obj = bookRepository.findById(id);
+
+		Book book = obj.orElseThrow(() -> new ResourceNotFoundException("Resource BOOK not found. ID " + id));
+
+		logger.info("Resource BOOK found: " + book.toString());
+
+		BookDTOv2 dto = BookMapper.convertEntityToDtoV2(book);
+
+		dto.add(linkTo(methodOn(BookController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+		dto.add(linkTo(methodOn(BookController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+		dto.add(linkTo(methodOn(BookController.class).update(dto.getId(), dto)).withRel("update").withType("PUT"));
+
+		return dto;
+
+	}
+
+	@CacheEvict(value = "books", allEntries = true)
+	@Transactional
+	public BookDTOv2 insert(BookDTOv2 dto) {
+
+		dto.setStatus(BookStatus.AVAILABLE);
+
+		dto.setAuthor(authorRepository.findById(dto.getAuthor().getId())
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Resource AUTHOR not found. ID " + dto.getAuthor().getId())));
+
+		Book entity = BookMapper.convertDtoToEntityV2(dto);
+
+		entity.getCategories().clear();
+
+		for (CategoryDTOv2 categoryDTO : dto.getCategories()) {
+
+			Category category = categoryRepository.findById(categoryDTO.getId())
+					.orElseThrow(() -> new ResourceNotFoundException(
+							"Resource CATEGORY not found. ID " + categoryDTO.getId()));
+
+			entity.getCategories().add(category);
+
+		}
+
+		entity = bookRepository.save(entity);
+
+		logger.info("Resource BOOK saved: " + entity.toString());
+
+		BookDTOv2 newDto = BookMapper.convertEntityToDtoV2(entity);
+
+		newDto.add(linkTo(methodOn(BookController.class).findById(newDto.getId())).withSelfRel().withType("GET"));
+		newDto.add(linkTo(methodOn(BookController.class).delete(newDto.getId())).withRel("delete").withType("DELETE"));
+		newDto.add(linkTo(methodOn(BookController.class).update(newDto.getId(), newDto)).withRel("update")
+				.withType("PUT"));
+
+		return newDto;
+
+	}
+
+	@Caching(evict = { @CacheEvict(value = "books", allEntries = true) }, put = {
+			@CachePut(value = "book", key = "#id") })
+	@Transactional
+	public BookDTOv2 update(Long id, BookDTOv2 dto) {
+
+		try {
+
+			Book entity = bookRepository.getReferenceById(id);
+
+			dto.setStatus(entity.getStatus());
+
+			dto.setId(entity.getId());
+
+			entity = BookMapper.convertDtoToEntityV2(dto);
+
+			entity = bookRepository.save(entity);
+
+			logger.info("Resource BOOK updated: " + entity.toString());
+
+			dto = BookMapper.convertEntityToDtoV2(entity);
+
+			dto.add(linkTo(methodOn(BookController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+			dto.add(linkTo(methodOn(BookController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+			dto.add(linkTo(methodOn(BookController.class).update(dto.getId(), dto)).withRel("update").withType("PUT"));
+
+			return dto;
+
+		} catch (EntityNotFoundException e) {
+
+			throw new ResourceNotFoundException("Resource BOOK not found. ID " + id);
+
+		}
+
+	}
+
+	@Caching(evict = {
+			@CacheEvict(value = "books", allEntries = true),
+			@CacheEvict(value = "book", key = "#id")
+	})
+	public void delete(Long id) {
+
+		try {
+
+			if (bookRepository.existsById(id)) {
+				bookRepository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException("Resource BOOK not found. ID " + id);
+			}
+
+			logger.info("Resource BOOK deleted: ID " + id);
+
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DatabaseException(e.getMessage());
+
+		}
+
+	}
+
+}
