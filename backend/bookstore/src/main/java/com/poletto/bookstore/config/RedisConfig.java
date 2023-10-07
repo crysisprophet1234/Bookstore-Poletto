83a394d0ad203871e0aeebdd2cb50cbf4d1d3f54
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-07 16:38:22 -0300

    Delegado cacheErrorHandler para RedisConfig e customHandler para exceptions #25

--- backend/bookstore/src/main/java/com/poletto/bookstore/config/RedisConfig.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/config/RedisConfig.java
@@ -1,31 +1,88 @@
 package com.poletto.bookstore.config;
 
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.hateoas.mediatype.MessageResolver;
+import org.springframework.hateoas.mediatype.hal.CurieProvider;
+import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
+import org.springframework.hateoas.mediatype.hal.Jackson2HalModule.HalHandlerInstantiator;
+import org.springframework.hateoas.server.LinkRelationProvider;
+import org.springframework.hateoas.server.core.AnnotationLinkRelationProvider;
+import org.springframework.hateoas.server.core.DelegatingLinkRelationProvider;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.poletto.bookstore.converter.CustomObjectMapper;
+import com.poletto.bookstore.exceptions.handler.CustomCacheExceptionHandler;
 
 @Configuration
-public class RedisConfig {
+public class RedisConfig implements CachingConfigurer  {
+
+	@SuppressWarnings("unused")
+	private GenericJackson2JsonRedisSerializer mapper() {
+
+		CustomObjectMapper objectMapper = new CustomObjectMapper();
+
+		objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), DefaultTyping.EVERYTHING, JsonTypeInfo.As.PROPERTY);
+		
+		LinkRelationProvider provider = new DelegatingLinkRelationProvider(new AnnotationLinkRelationProvider());
+
+		objectMapper.registerModule(new Jackson2HalModule());
+		objectMapper.setHandlerInstantiator(
+				new HalHandlerInstantiator(provider, CurieProvider.NONE, MessageResolver.DEFAULTS_ONLY));
+		
+		return new GenericJackson2JsonRedisSerializer(objectMapper);
+		
+	}
 
 	@Bean
 	RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
+		//template.setValueSerializer(mapper());
 		template.setValueSerializer(new JdkSerializationRedisSerializer());
 		return template;
 	}
 
-//	@Bean
-//	RedisCacheConfiguration cacheConfiguration() {
-//		CustomObjectMapper objectMapper = new CustomObjectMapper();
-//		objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.EVERYTHING, JsonTypeInfo.As.PROPERTY);
-//		return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(60)).disableCachingNullValues()
-//				.serializeValuesWith(
-//						SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
-//	}
+	@Bean
+	CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheManager redisCacheManager = new RedisCacheManager(
+				RedisCacheWriter.lockingRedisCacheWriter(connectionFactory),
+				cacheConfiguration()
+		);
+		return redisCacheManager;
+	}
+
+	@Bean
+	RedisCacheConfiguration cacheConfiguration() {
+		return RedisCacheConfiguration.defaultCacheConfig()
+				.entryTtl(Duration.ofMinutes(60))
+				.disableCachingNullValues()
+				.serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+				//.serializeValuesWith(SerializationPair.fromSerializer(mapper()));
+				.serializeValuesWith(SerializationPair.fromSerializer(new JdkSerializationRedisSerializer()));
+	}
+	
+	@Bean
+	public CacheErrorHandler errorHandler() {
+
+		return new CustomCacheExceptionHandler();
+		
+	}
 
 }
---
+++ backend/bookstore/src/main/java/com/poletto/bookstore/exceptions/handler/CustomCacheExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.poletto.bookstore.exceptions.handler;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.core.env.Environment;
+
+import com.poletto.bookstore.services.v2.EmailService;
+import com.poletto.bookstore.util.StackTraceFormatter;
+
+public class CustomCacheExceptionHandler implements CacheErrorHandler {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomCacheExceptionHandler.class);
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private EmailService emailService;
+
+	@Override
+	public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+		logger.warn("cache failure trying to insert cache: {}", exception.getMessage());
+		exceptionHandling(exception);
+	}
+
+	@Override
+	public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+		logger.warn("cache failure trying to get cache: {}", exception.getMessage());
+		exceptionHandling(exception);
+	}
+
+	@Override
+	public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+		logger.warn("cache failure trying to evict cache: {}", exception.getMessage());
+		exceptionHandling(exception);
+
+	}
+
+	@Override
+	public void handleCacheClearError(RuntimeException exception, Cache cache) {
+		logger.warn("cache failure trying to clear cache: {}", exception.getMessage());
+		exceptionHandling(exception);
+	}
+	
+	private void exceptionHandling(Exception ex) {
+		
+		logger.error(StackTraceFormatter.stackTraceFormatter(ex));
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+			
+			emailService.sendEmail(
+					"polettobookstore@gmail.com",
+					"Redis server failure on production enviroment",
+					"Main instance of poletto-bookstore API unnable to connect to Redis Server in production enviroment at " + LocalDateTime.now()
+				  + "\nFollows below the stack trace for the error:"
+				  + "\n\n" + StackTraceFormatter.stackTraceFormatter(ex)
+			);
+
+		}
+
+	}
+
+}
