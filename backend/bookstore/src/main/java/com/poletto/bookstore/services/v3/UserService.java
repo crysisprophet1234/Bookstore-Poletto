dd90c82a9b07d7af76ceea33441a0743fb8fef2c
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-13 22:35:54 -0300

    Versionado service e controller para v3 com caching, testes passando.
    Adicionando a versão 3 da UserService, sem os metódos de autenticação e
    registro agora que estão separados na AuthService.

---
+++ backend/bookstore/src/main/java/com/poletto/bookstore/services/v3/UserService.java
@@ -0,0 +1,191 @@
+package com.poletto.bookstore.services.v3;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.poletto.bookstore.controllers.v2.UserController;
+import com.poletto.bookstore.converter.custom.UserMapper;
+import com.poletto.bookstore.dto.v2.RoleDTOv2;
+import com.poletto.bookstore.dto.v2.UserAuthDTOv2;
+import com.poletto.bookstore.dto.v2.UserDTOv2;
+import com.poletto.bookstore.entities.Reservation;
+import com.poletto.bookstore.entities.User;
+import com.poletto.bookstore.exceptions.DatabaseException;
+import com.poletto.bookstore.exceptions.ResourceNotFoundException;
+import com.poletto.bookstore.repositories.v2.RoleRepository;
+import com.poletto.bookstore.repositories.v2.UserRepository;
+import com.poletto.bookstore.util.CustomRedisClient;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service("UserServiceV3")
+public class UserService {
+
+	//criar classes de teste e validar interação com outras entidades (reservation.client basicamente)
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private CustomRedisClient<String, Object> redisClient;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Cacheable("users")
+	@Transactional(readOnly = true)
+	public Page<UserDTOv2> findAllPaged(Pageable pageable) {
+
+		Page<User> userPage = userRepository.findAll(pageable);
+
+		logger.info("Resource USER page found: " + "PAGE NUMBER [" + userPage.getNumber() + "] - CONTENT: "
+				+ userPage.getContent());
+		
+		Page<UserDTOv2> dtos = userPage.map(x -> UserMapper.convertEntityToDtoV2(x));
+		
+		dtos.stream().forEach(x -> x
+				.add(linkTo(methodOn(UserController.class).findById(x.getId())).withSelfRel().withType("GET"))
+				.add(linkTo(methodOn(UserController.class).delete(x.getId())).withRel("delete").withType("DELETE"))
+				.add(linkTo(methodOn(UserController.class).update(x.getId(), UserMapper.convertEntityToAuthDtoV2(UserMapper.convertDtoToEntityV2(x)))).withRel("update").withType("PUT")));
+
+		return dtos;
+
+	}
+
+	@Cacheable(value = "user", key = "#id")
+	@Transactional(readOnly = true)
+	public UserDTOv2 findById(Long id) {
+
+		Optional<User> user = userRepository.findById(id);
+
+		User entity = user.orElseThrow(() -> new ResourceNotFoundException("Resource USER not found. ID " + id));
+
+		logger.info("Resource USER found: " + entity.toString());
+		
+		UserDTOv2 dto = UserMapper.convertEntityToDtoV2(entity);
+		
+		dto.add(linkTo(methodOn(UserController.class).findById(dto.getId())).withSelfRel().withType("GET"));
+		dto.add(linkTo(methodOn(UserController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
+		dto.add(linkTo(methodOn(UserController.class).update(dto.getId(), UserMapper.convertEntityToAuthDtoV2(UserMapper.convertDtoToEntityV2(dto)))).withRel("update").withType("PUT"));
+
+		return dto;
+
+	}
+
+	// TODO userAuth here??? frontend will eventually have person/user changes
+	@Caching(
+			evict = { @CacheEvict(value = {"users", "reservations", "reservation"}, allEntries = true) },
+			put = { @CachePut(value = "user", key = "#id") }
+	)
+	@Transactional
+	public UserDTOv2 update(Long id, UserAuthDTOv2 dto) {
+
+			User entity = userRepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Resource USER not found. ID: " + id));
+			
+			String previousEmail = entity.getEmail();
+			
+			List<Reservation> reservations = entity.getOrders();
+
+			entity = UserMapper.convertDtoToEntityV2(dto);
+
+			entity.setId(id);
+			
+			entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+			entity.getRoles().clear();
+
+			for (RoleDTOv2 roleDTO : dto.getRoles()) {
+				
+				//TODO catch and throw?
+
+				try {
+
+					entity.getRoles().add(roleRepository.getReferenceById(roleDTO.getId()));
+
+				} catch (EntityNotFoundException e) {
+
+					throw new ResourceNotFoundException("Resource ROLE not found. ID " + roleDTO.getId());
+
+				}
+			}
+
+			entity = userRepository.save(entity);
+
+			if (redisClient.del("userAuth::" + previousEmail)) {
+				logger.info("Cache userAuth evicted, e-mail: {}", previousEmail);
+			}
+			
+			for (Reservation res: reservations) {
+				res.setClient(entity);
+				if (redisClient.put("reservation::" + res.getId(), res)) {
+					logger.info("Cache reservation::{} updated client to: {}", res.getId(), res);
+				}
+			}
+			
+			UserDTOv2 newDto = UserMapper.convertEntityToDtoV2(entity);
+			
+			logger.info("Resource USER updated: {}", newDto);
+			
+			newDto.add(linkTo(methodOn(UserController.class).findById(newDto.getId())).withSelfRel().withType("GET"));
+			newDto.add(linkTo(methodOn(UserController.class).delete(newDto.getId())).withRel("delete").withType("DELETE"));
+			newDto.add(linkTo(methodOn(UserController.class).update(newDto.getId(), UserMapper.convertEntityToAuthDtoV2(UserMapper.convertDtoToEntityV2(newDto)))).withRel("update").withType("PUT"));
+
+			return newDto;
+
+	}
+
+	@Caching(evict = { 
+			@CacheEvict(value = "users", allEntries = true),
+			@CacheEvict(value = "user", key = "#id")
+	})
+	@Transactional
+	public void delete(Long id) {
+
+		try {
+			
+			String email = userRepository.getReferenceById(id).getEmail();
+			
+			userRepository.deleteById(id);
+			
+			if (redisClient.del("userAuth::" + email)) {				
+				logger.info("Cache userAuth evicted, id: {} ", id);		
+			}
+			
+			logger.info("Resource USER deleted, id: {}", id);
+
+		} catch (EntityNotFoundException e) {
+
+			throw new ResourceNotFoundException("Resource USER not found. ID " + id);
+
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DatabaseException("Integrity violation");
+
+		}
+
+	}
+
+}
---
+++ backend/bookstore/src/test/java/com/poletto/bookstore/v3/services/UserServiceTest.java
@@ -0,0 +1,320 @@
+package com.poletto.bookstore.v3.services;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.poletto.bookstore.dto.v2.UserAuthDTOv2;
+import com.poletto.bookstore.dto.v2.UserDTOv2;
+import com.poletto.bookstore.repositories.v2.UserRepository;
+import com.poletto.bookstore.services.v3.AuthService;
+import com.poletto.bookstore.services.v3.UserService;
+import com.poletto.bookstore.util.CustomRedisClient;
+import com.poletto.bookstore.v3.mocks.UserMocks;
+
+import redis.embedded.RedisServer;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestInstance(Lifecycle.PER_CLASS)
+public class UserServiceTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
+
+	@Autowired
+	private CustomRedisClient<String, UserDTOv2> client;
+
+	private static RedisServer redisServer;
+
+	@SpyBean
+	private UserRepository userRepository;
+
+	@Autowired
+	@InjectMocks
+	private UserService userService;
+
+	@Autowired
+	@InjectMocks
+	private AuthService authService;
+
+	UserDTOv2 userDto;
+	UserAuthDTOv2 insertUserDto;
+
+	@BeforeAll
+	public void startRedis() {
+
+		try {
+			redisServer = RedisServer.builder().port(6370).setting("maxmemory 128M").build();
+			redisServer.start();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+	}
+
+	@BeforeEach
+	public void setUp() {
+
+		userDto = UserMocks.userMockDto();
+		insertUserDto = UserMocks.registerUserMockDto();
+
+	}
+
+	@AfterEach
+	public void tearDown() {
+
+		userDto = insertUserDto = null;
+
+		client.clear();
+
+	}
+
+	@AfterAll
+	public void cleanUp() {
+		redisServer.stop();
+	}
+
+	@Test
+	void isUserBeingCachedAfterQueryById() {
+
+		logger.info("\n\n<=========  STARTING TEST isUserBeingCachedAfterQueryById()  =========>\n");
+
+//		logger.info("inserting a new user");
+//		
+//		assertDoesNotThrow(() -> userDto = authService.register(insertUserDto), "error thrown when trying to insert new user");
+
+		logger.info("querying user with id 1 on the repository");
+
+		assertDoesNotThrow(() -> userDto = userService.findById(1L),
+				"error thrown when trying to query user with id 1");
+
+		logger.info("querying user with id 1 on the cache");
+
+		UserDTOv2 cachedUserDto = client.get("user::" + 1L);
+
+		assertNotNull(cachedUserDto, "cachedUserDto was null");
+
+		logger.info("asserting that userDto is equals to cachedUserDto");
+
+		assertEquals(userDto, cachedUserDto, "userDto and cachedUserDto were not equals");
+
+		logger.info("querying user with id 1 on the repository again to check if cache gets hit");
+
+		assertDoesNotThrow(() -> userDto = userService.findById(1L),
+				"error thrown when trying to query user with id 1");
+
+		logger.info("asserting that new userDto is equals to cachedUserDto");
+
+		assertEquals(userDto, cachedUserDto, "new userDto and cachedUserDto were not equals");
+
+		logger.info("asserting that repository was invoked only once");
+
+		verify(userRepository, times(1)).findById(1L);
+
+		logger.info("test success, user with id 1 got properly cached and repository was only invoked one time");
+
+	}
+
+	@Test
+	void isUserPageBeingCachedAfterPagedQuery() {
+
+		logger.info("\n\n<=========  STARTING TEST isUserPageBeingCachedAfterPagedQuery()  =========>\n");
+
+		logger.info("querying user page from repository");
+
+		Page<UserDTOv2> userPage = findPageOfUsersFromService();
+
+		assertNotNull(userPage, "userPage was null");
+
+		logger.info("querying user page from cache");
+
+		Page<UserDTOv2> cachedUserPage = findPageOfUsersFromCache();
+
+		assertNotNull(cachedUserPage, "cachedUserPage was null");
+
+		logger.info("asserting userPage from repository and cache are equals");
+
+		assertEquals(userPage.getContent(), cachedUserPage.getContent(),
+				"userPage from repository and cache were not equals");
+
+		logger.info("querying user page on the repository again to check if cache gets hit");
+
+		userPage = findPageOfUsersFromService();
+
+		logger.info("asserting that the updated userPage is equals to cachedUserPage");
+
+		assertEquals(userPage, cachedUserPage, "updated userPage and cachedUserPage were not equals");
+
+		logger.info("asserting that repository was invoked only once");
+
+		verify(userRepository, times(1)).findAll(any(Pageable.class));
+
+		logger.info("test success, userPage got properly cached and repository was only invoked one time");
+
+	}
+
+	@Test
+	void isUserCacheBeingUpdated() {
+
+		logger.info("\n\n<=========  STARTING TEST isUserCacheBeingUpdated()  =========>\n");
+
+//		logger.info("inserting a new user");
+//		
+//		assertDoesNotThrow(() -> userDto = authService.register(insertUserDto), "error thrown when trying to insert new user");
+//		
+//		Long 1 = userDto.getId();
+
+		logger.info("querying user with {} on the repository", 1);
+
+		assertDoesNotThrow(() -> userDto = userService.findById(1L),
+				"error thrown when trying to query user with id " + 1);
+
+		logger.info("querying user with id {} on the cache", 1);
+
+		UserDTOv2 cachedUserDto = client.get("user::" + 1);
+
+		assertNotNull(cachedUserDto, "cachedUserDto was null");
+
+		logger.info("asserting that userDto is equals to cachedUserDto");
+
+		assertEquals(userDto, cachedUserDto, "userDto and cachedUserDto were not equals");
+
+		logger.info("updating user with id {}", 1);
+
+		UserAuthDTOv2 updateUserDto = UserMocks.userAuthDto(1L);
+
+		updateUserDto.setFirstname("Newname");
+
+		assertDoesNotThrow(() -> userDto = userService.update(1L, updateUserDto),
+				"error thrown when trying to update user with id " + 1);
+
+		logger.info("asserting that updated userDto is not equals to previous cached userDto");
+
+		assertNotEquals(userDto, cachedUserDto, "updated userDto was equals to previous cached userDto");
+
+		logger.info("querying user with id {} again", 1);
+
+		assertDoesNotThrow(() -> userDto = userService.findById(1L),
+				"error thrown when trying to query user with id " + 1);
+
+		logger.info("querying user with id {} on the cache again", 1);
+
+		cachedUserDto = client.get("user::" + 1);
+
+		logger.info("asserting that the cache got properly updated");
+
+		assertEquals(userDto, cachedUserDto, "updated userDto was not equals to updated cached userDto");
+
+		logger.info("asserting that the repository was invoked only once");
+
+		verify(userRepository, times(2)).findById(1L);
+
+		logger.info(
+				"test success, user with id {} got properly cached and updated, repository was only invoked one time",
+				1);
+
+	}
+
+	@Test
+	void isUserPageAndSingleCacheGettingEvictedAfterDeletion() {
+
+		logger.info(
+				"\n\n<=========  STARTING TEST isUserPageAndSingleCacheGettingEvictedAfterDeletion()  =========>\n");
+
+		logger.info("querying user page from repository");
+
+		Page<UserDTOv2> userPage = findPageOfUsersFromService();
+
+		assertNotNull(userPage, "userPage was null");
+
+		logger.info("querying user page from cache");
+
+		Page<UserDTOv2> cachedUserPage = findPageOfUsersFromCache();
+
+		assertNotNull(cachedUserPage, "cachedUserPage was null");
+
+		logger.info("asserting userPage from repository and cache are equals");
+
+		assertEquals(userPage.getContent(), cachedUserPage.getContent(),
+				"userPage from repository and cache were not equals");
+
+		logger.info("querying user with id 1 on the repository");
+
+		assertDoesNotThrow(() -> userDto = userService.findById(1L),
+				"error thrown when trying to query user with id 1");
+
+		logger.info("querying user with id 1 on the cache");
+
+		UserDTOv2 cachedUserDto = client.get("user::" + 1L);
+
+		assertNotNull(cachedUserDto, "cachedUserDto was null");
+
+		logger.info("asserting that userDto is equals to cachedUserDto");
+
+		assertEquals(userDto, cachedUserDto, "userDto and cachedUserDto were not equals");
+
+		logger.info("deleting user with id 1");
+
+		assertDoesNotThrow(() -> userService.delete(1L), "user deletion threw an exception");
+
+		logger.info("asserting that the single user cache got evicted");
+
+		assertNull(client.get("user::" + 1L), "single user cache didnt got evicted");
+
+		logger.info("asserting that the user page cache got evicted");
+
+		assertNull(findPageOfUsersFromCache(), "user page cache didnt got evicted");
+
+		logger.info("test success, user page and user deleted cache got evicted after user deletion");
+
+	}
+
+	private Page<UserDTOv2> findPageOfUsersFromService() {
+
+		Pageable pageable = PageRequest.of(0, 20);
+
+		return userService.findAllPaged(pageable);
+
+	}
+
+	private Page<UserDTOv2> findPageOfUsersFromCache() {
+
+		String expectedCacheKey = "users::Page request [number: 0, size 20, sort: UNSORTED]";
+
+		try {
+			return ((Page<?>) client.get(expectedCacheKey)).map(x -> (UserDTOv2) x);
+		} catch (NullPointerException ex) {
+			return null;
+		}
+
+	}
+
+}
