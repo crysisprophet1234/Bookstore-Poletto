3c9f5b540b3f330f154a1bd65dff1aeb6c969430
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-14 17:11:05 -0300

    Versionado service e controller para v3 com caching, testes passando #20
    Adicionando a versão 3 da AuthorService, na controller agora a rota de
    chamada para a lista com todos autores é apenas /authors/v3, se o client
    quiser a página de autores deve chamar /authors/v3/paged. Padronizado
    nomenclatura na execução dos serviços, corrigido links de livros por
    autor.

---
+++ backend/bookstore/src/main/java/com/poletto/bookstore/services/v3/AuthorService.java
@@ -0,0 +1,83 @@
+package com.poletto.bookstore.services.v3;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.poletto.bookstore.controllers.v2.AuthorController;
+import com.poletto.bookstore.controllers.v2.BookController;
+import com.poletto.bookstore.converter.DozerMapperConverter;
+import com.poletto.bookstore.dto.v2.AuthorDTOv2;
+import com.poletto.bookstore.entities.Author;
+import com.poletto.bookstore.exceptions.ResourceNotFoundException;
+import com.poletto.bookstore.repositories.v2.AuthorRepository;
+
+@Service("AuthorServiceV3")
+public class AuthorService {
+
+	private static final Logger logger = LoggerFactory.getLogger(AuthorService.class);
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Cacheable(value = "authorsList")
+	@Transactional(readOnly = true)
+	public List<AuthorDTOv2> findAll() {
+
+		List<Author> entitiesList = authorRepository.findAll();
+
+		logger.info("Resoucer AUTHOR list found: " + entitiesList);
+
+		List<AuthorDTOv2> dtosList = DozerMapperConverter.parseListObjects(entitiesList, AuthorDTOv2.class);
+
+		dtosList.stream().forEach(dto -> dto
+				.add(linkTo(methodOn(AuthorController.class).findById(dto.getId())).withSelfRel().withType("GET"))
+				.add(linkTo(methodOn(BookController.class).findAllPaged(0, 12, "asc", "name", null, dto.getName(), "all")).withRel("BOOKS BY AUTHOR").withType("GET")));
+
+		return dtosList;
+
+	}
+
+	@Cacheable(value = "authorsPage")
+	@Transactional(readOnly = true)
+	public Page<AuthorDTOv2> findAllPaged(Pageable pageable) {
+
+		Page<Author> entitiesPage = authorRepository.findAll(pageable);
+
+		logger.info("Resource AUTHOR page found: " + "PAGE NUMBER [" + entitiesPage.getNumber() + "] - CONTENT: " + entitiesPage.getContent());
+
+		Page<AuthorDTOv2> dtosPage = entitiesPage.map(x -> DozerMapperConverter.parseObject(x, AuthorDTOv2.class));
+
+		dtosPage.stream().forEach(dto -> dto
+				.add(linkTo(methodOn(AuthorController.class).findById(dto.getId())).withSelfRel().withType("GET"))
+				.add(linkTo(methodOn(BookController.class).findAllPaged(0, 12, "asc", "name", null, dto.getName(), "all")).withRel("BOOKS BY AUTHOR").withType("GET")));
+
+		return dtosPage;
+
+	}
+
+	@Cacheable(value = "author", key = "#id")
+	@Transactional(readOnly = true)
+	public AuthorDTOv2 findById(Long id) {
+
+		Author entity = authorRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Resource AUTHOR not found. ID " + id));
+
+		logger.info("Resource AUTHOR found: " + entity.toString());
+
+		return DozerMapperConverter.parseObject(entity, AuthorDTOv2.class)
+				.add(linkTo(methodOn(BookController.class).findAllPaged(0, 12, "asc", "name", null, entity.getName(), "all")).withRel("BOOKS BY AUTHOR").withType("GET"));
+
+	}
+
+}
---
+++ backend/bookstore/src/test/java/com/poletto/bookstore/v3/services/AuthorServiceTest.java
@@ -0,0 +1,159 @@
+package com.poletto.bookstore.v3.services;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.poletto.bookstore.dto.v2.AuthorDTOv2;
+import com.poletto.bookstore.repositories.v2.AuthorRepository;
+import com.poletto.bookstore.services.v3.AuthorService;
+import com.poletto.bookstore.util.CustomRedisClient;
+
+import redis.embedded.RedisServer;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestInstance(Lifecycle.PER_CLASS)
+public class AuthorServiceTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AuthorServiceTest.class);
+	
+	@Autowired
+	private CustomRedisClient<String , AuthorDTOv2> client;
+
+	private static RedisServer redisServer;
+
+	@SpyBean
+	private AuthorRepository repository;
+
+	@Autowired
+	@InjectMocks
+	private AuthorService service;
+	
+	AuthorDTOv2 authorDto;
+	
+	@BeforeAll
+	public void startRedis() {
+
+		try {
+			redisServer = RedisServer.builder().port(6370).setting("maxmemory 128M").build();
+			redisServer.start();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+	}
+	
+	@BeforeEach
+	public void setUp() {
+		authorDto = new AuthorDTOv2();
+	}
+	
+	@AfterAll
+	public void cleanUp() {
+		authorDto = null;
+		redisServer.stop();
+	}
+	
+	@Test
+	void isAuthorBeingCachedAfterQueryById() {
+		
+		logger.info("\n\n<=========  STARTING TEST isAuthorBeingCachedAfterQueryById()  =========>\n");
+		
+		logger.info("querying author with id 1 on the repository");
+
+		assertDoesNotThrow(() -> authorDto = service.findById(1L),
+				"error thrown when trying to query author with id 1");
+
+		logger.info("querying author with id 1 on the cache");
+
+		AuthorDTOv2 cachedAuthorDto = client.get("author::" + 1L);
+
+		assertNotNull(cachedAuthorDto, "cachedAuthorDto was null");
+
+		logger.info("asserting that authorDto is equals to cachedAuthorDto");
+
+		assertEquals(authorDto, cachedAuthorDto, "authorDto and cachedAuthorDto were not equals");
+
+		logger.info("querying author with id 1 on the repository again to check if cache gets hit");
+
+		assertDoesNotThrow(() -> authorDto = service.findById(1L),
+				"error thrown when trying to query author with id 1");
+
+		logger.info("asserting that new authorDto is equals to cachedAuthorDto");
+
+		assertEquals(authorDto, cachedAuthorDto, "new authorDto and cachedAuthorDto were not equals");
+
+		logger.info("asserting that repository was invoked only once");
+
+		verify(repository, times(1)).findById(1L);
+
+		logger.info("test success, author with id 1 got properly cached and repository was only invoked one time");	
+		
+	}
+	
+	@Test
+	void isAuthorListBeingCachedAfterQueryAll() {
+
+		logger.info("\n\n<=========  STARTING TEST isAuthorListBeingCachedAfterQueryAll()  =========>\n");
+		
+		String expectedCacheKey = "authorsList::SimpleKey []";
+
+		logger.info("querying author list from repository");
+
+		List<AuthorDTOv2> authorList = service.findAll();
+
+		assertNotNull(authorList, "authorList was null");
+
+		logger.info("querying author list from cache");
+
+		List<AuthorDTOv2> cachedAuthorList = ((List<?>) client.get(expectedCacheKey)).stream().map(x -> (AuthorDTOv2) x).collect(Collectors.toList());
+
+		assertNotNull(cachedAuthorList, "cachedAuthorList was null");
+
+		logger.info("asserting authorList from repository and cache are equals");
+
+		assertEquals(authorList, cachedAuthorList, "authorList from repository and cache were not equals");
+
+		logger.info("querying author list on the repository again to check if cache gets hit");
+
+		authorList = service.findAll();
+
+		logger.info("asserting that the updated authorList is equals to cachedAuthorList");
+
+		assertEquals(authorList, cachedAuthorList, "updated authorList and cachedAuthorList were not equals");
+
+		logger.info("asserting that repository was invoked only once");
+
+		verify(repository, times(1)).findAll();
+
+		logger.info("test success, authorList got properly cached and repository was only invoked one time");
+
+	}
+	
+
+}
