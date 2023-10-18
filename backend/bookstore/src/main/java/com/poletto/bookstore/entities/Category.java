3db7dd062991c61f9d3a02128979b57254a2bb9e
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-10-17 23:59:41 -0300

    Overriding comparador de objetos e modificado toString().

--- backend/bookstore/src/main/java/com/poletto/bookstore/entities/Category.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/entities/Category.java
@@ -2,7 +2,6 @@ package com.poletto.bookstore.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -59,7 +58,11 @@ public class Category implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
 	}
 
 	@Override
@@ -71,12 +74,22 @@ public class Category implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Category [id=" + id + ", name=" + name + ", books=" + books + "]";
-	}
+		return "Category [id=" + id + ", name=" + name + "]";
+	}	
 	
 }
