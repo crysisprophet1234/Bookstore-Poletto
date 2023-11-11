c0cc0b77f6c4af2852c39553147ef1fd5da8ab33
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-11-10 21:50:24 -0300

    Removido envio de e-mail na exceção de cache em prod.

--- backend/bookstore/src/main/java/com/poletto/bookstore/exceptions/handler/CustomCacheExceptionHandler.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/exceptions/handler/CustomCacheExceptionHandler.java
@@ -1,27 +1,21 @@
 package com.poletto.bookstore.exceptions.handler;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.core.env.Environment;
 
-import com.poletto.bookstore.services.v2.EmailService;
 import com.poletto.bookstore.util.StackTraceFormatter;
 
 public class CustomCacheExceptionHandler implements CacheErrorHandler {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CustomCacheExceptionHandler.class);
 
-	@Autowired
-	private Environment env;
+	//@Autowired
+	//private Environment env;
 	
-	@Autowired
-	private EmailService emailService;
+	//@Autowired
+	//private EmailService emailService;
 
 	@Override
 	public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
@@ -52,17 +46,17 @@ public class CustomCacheExceptionHandler implements CacheErrorHandler {
 		
 		logger.error(StackTraceFormatter.stackTraceFormatter(ex));
 
-		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
-			
-			emailService.sendEmail(
-					"polettobookstore@gmail.com",
-					"Redis server failure on production enviroment",
-					"Main instance of poletto-bookstore API unnable to connect to Redis Server in production enviroment at " + LocalDateTime.now()
-				  + "\nFollows below the stack trace for the error:"
-				  + "\n\n" + StackTraceFormatter.stackTraceFormatter(ex)
-			);
-
-		}
+//		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+//			
+//			emailService.sendEmail(
+//					"polettobookstore@gmail.com",
+//					"Redis server failure on production enviroment",
+//					"Main instance of poletto-bookstore API unnable to connect to Redis Server in production enviroment at " + LocalDateTime.now()
+//				  + "\nFollows below the stack trace for the error:"
+//				  + "\n\n" + StackTraceFormatter.stackTraceFormatter(ex)
+//			);
+//
+//		}
 
 	}
 
