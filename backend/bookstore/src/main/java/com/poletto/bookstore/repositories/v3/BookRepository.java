f9a501c6a50de4add20447061a851c37ae252c58
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-01-27 10:00:22 -0300

    Adaptando anotações de caching e modificando query de findPaged
    Com as alterações de nomenclatura das propriedades da entidade Book #12
    e #8 é necessário alterar as querys personalizadas na classe de
    repositório.

--- backend/bookstore/src/main/java/com/poletto/bookstore/repositories/v3/BookRepository.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/repositories/v3/BookRepository.java
@@ -3,9 +3,7 @@ package com.poletto.bookstore.repositories.v3;
 import java.util.Optional;
 
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,36 +16,24 @@ import com.poletto.bookstore.entities.Book;
 @Repository("BookRepositoryV3")
 public interface BookRepository extends JpaRepository<Book, Long> {
 	
-	@Cacheable(value = "books")
-	@Query("SELECT DISTINCT obj "
+	@Query("SELECT obj "
 		 + "FROM Book obj "
 		 + "INNER JOIN obj.categories cats "
 		 + "WHERE (:categoryId IS NULL OR cats.id IN :categoryId) "
-		 + "AND (:name IS NULL OR LOWER(obj.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+		 + "AND (:name IS NULL OR LOWER(obj.title) LIKE LOWER(CONCAT('%', :name, '%')) "
 		 + "OR LOWER(obj.author.name) LIKE LOWER(CONCAT('%', :name, '%'))) "
-		 + "AND (:status = 'ALL' OR obj.status = :status)")
+		 + "AND (LOWER(:status) = 'all' OR LOWER(cast(obj.status as text)) = LOWER(:status))")
 	Page<Book> findPaged(
-	        @Param("categoryId") Long categoryId,
-	        @Param("name") String name,
-	        @Param("status") String status,
-	        Pageable pageable
+        @Param("categoryId") Long categoryId,
+        @Param("name") String name,
+        @Param("status") String status,
+        Pageable pageable
 	);
 	
 	@Cacheable(value = "book", key = "#id")
 	Optional<Book> findById(Long id);
 	
-	@Caching(
-			evict = { @CacheEvict(value = "books", allEntries = true) },
-			put = { @CachePut(value = "book", key = "#entity.id", condition = "#entity.id != null") }
-	)
+	@CacheEvict(value = "book", key = "#entity.id")
 	<S extends Book> S save(S entity);
-	
-	@Caching(evict = {
-			@CacheEvict(value = "books", allEntries = true),
-			@CacheEvict(value = "book", key = "#id")
-	})
-	void deleteById(Long id);
-	
-	
 	
 }
