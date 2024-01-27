15faf0d060a31ad7232015b9df3928de00bd1c62
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2024-01-27 10:01:07 -0300

    Adicionado overload para casos de Book Status inválido.

--- backend/bookstore/src/main/java/com/poletto/bookstore/exceptions/InvalidStatusException.java
+++ backend/bookstore/src/main/java/com/poletto/bookstore/exceptions/InvalidStatusException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.poletto.bookstore.entities.Book;
 import com.poletto.bookstore.entities.Reservation;
+import com.poletto.bookstore.entities.User;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidStatusException extends RuntimeException {
@@ -12,11 +13,18 @@ public class InvalidStatusException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidStatusException(Book book) {
-		super("Invalid status for book ID " + book.getId());
+		super("Status inválido para o livro ID " + book.getId() + ", permanecerá o status " + book.getStatus());
 	}
 
 	public InvalidStatusException(Reservation reservation) {
 		super("Invalid status for reservation ID " + reservation.getId());
 	}
-
+	
+	public InvalidStatusException(User user) {
+		super("Status inválido para o usuário ID " + user.getId() + ", permanecerá o status " + user.getUserStatus());
+	}
+	
+	public InvalidStatusException(Long userId) {
+		super("Usuário ID " + userId + " já está verificado");
+	}
 }
